9457a69b2f7eb0985cac35df5dd49f0a374497d6
Luis Abraham <luis.abrahame2006@gmail.com>
2025-04-17 18:12:54 -0400

    Removed Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-  public static void main (String args[]) {
-    byte alice = 'a';
-    char bob = 'c';
-
-    System.out.println((char) (((alice ^ 0xff) & 0xff) ^ 0xff));
-    System.out.println((char)((byte) bob));
-  }
-}
